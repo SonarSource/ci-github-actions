9dcd7fcbb01774046982ed9138f67af2ca91df26
Mikołaj Matuszny <mikolaj.matuszny.ext@sonarsource.com>
2025-08-15 13:12:13 +0200

    feat: Update spec, refactor, bugfix

--- build-maven/build.sh
+++ build-maven/build.sh
@@ -208,16 +208,9 @@ build_maven() {
 
   set_project_version
 
-  # Common Maven flags (made global for use in run_sonar_scanner)
-  readonly COMMON_MVN_FLAGS=(
-    "-Dmaven.test.redirectTestOutputToFile=false"
-    "--settings" "$MAVEN_SETTINGS"
-    "--batch-mode"
-    "--no-transfer-progress"
-    "--errors"
-    "--fail-at-end"
-    "--show-version"
-  )
+  # Source common Maven flags (made global for use in run_sonar_scanner)
+  # shellcheck source=maven-flags.sh
+  source "$(dirname "${BASH_SOURCE[0]}")/maven-flags.sh"
 
   local maven_command_args
   local enable_sonar=false
